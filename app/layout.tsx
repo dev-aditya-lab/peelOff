141255bb12b8f9ddd427e172fe66da74ce4cfce4
aadii <ad1123itya@gmail.com>
2025-06-06 21:31:54 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
       <body
         className={cn("min-h-screen bg-white font-sans antialiased")}>
           <MyNavbar/>
+          <Analytics/>
         {children}
+
         <FooterSection/>
       </body>
     </html>
